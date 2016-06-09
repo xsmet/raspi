f2860ae28d7574b862f51ceda696b6fc39bd4818
Xavier Smet <xaviersmet@gmail.com>
2016-06-10 00:07:49 +0200

    ACK direct message received

--- dmreceiver.py
+++ dmreceiver.py
@@ -10,9 +10,28 @@ auth = twitter.OAuth(
 	token_secret=ACCESSSECRET
 )
 
+twitter_api = twitter.Twitter(auth=auth)
+#Test authentication (should return an object)
+#print twitter_api
+
 stream = twitter.stream.TwitterStream(auth=auth, domain='userstream.twitter.com')
 
+msgid = ''
 for msg in stream.user():
 	if 'direct_message' in msg:
-		with open('twitterstream_dm_in.txt', 'a') as tf:
-			tf.write(msg['direct_message']['text'] + '\n')
+		# is there no ID that uniquely identifies this message in the stream? id_str changes continuously...
+		newmsgid = msg['direct_message']['created_at'] + msg['direct_message']['text'] + msg['direct_message']['sender_screen_name']
+		if msgid != newmsgid:
+			print msgid + " -- " + newmsgid
+			msgid = newmsgid
+			user = msg['direct_message']['sender_screen_name']
+			message = msg['direct_message']['text']
+			datum = msg['direct_message']['created_at']
+			line = datum + '\t' + user + ':\t' + message + '\n'
+			with open('twitterstream_dm_in.txt', 'a') as tf:
+				tf.write(line)
+			print line
+			try:
+				twitter_api.direct_messages.new(user=user, text="ACK")
+			except twitter.api.TwitterHTTPError as e:
+				print("DM failed: %s" % (str(e)))
